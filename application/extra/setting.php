2f2243121ec46700a5c6944cf80d078d7caeec3e
邱佳飞 <253896514@qq.com>
2019-01-15 11:06:34 +0800

    git add .

---
+++ application/extra/setting.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 2019/1/15
+ * Time: 9:46
+ */
+
+return [
+    'img_prefix' => 'http://www.51fafafa.cn'
+];
--- application/route.php
+++ application/route.php
@@ -8,14 +8,7 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
+use think\Route;
 
-return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
-
-];
+//Banner
+Route::get('api/:version/banner/:id', 'api/:version.Banner/getBanner');
